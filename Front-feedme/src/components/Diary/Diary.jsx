4bba3107ec2568a1fc10ce4e1012e38c49b698a4
진현지 <jinhyunji520@gmail.com>
2024-08-05 13:58:22 +0900

    Design: modal ing...

--- Front-feedme/src/components/Diary/Diary.jsx
+++ Front-feedme/src/components/Diary/Diary.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import Sidebar from '../Main/Sidebar';
-import SearchBar from '../Main/Search';
-import DiaryPic from './DiaryPic';
-import './Diary.css'
+import React from "react";
+import Sidebar from "../Main/Sidebar";
+import SearchBar from "../Main/Search";
+import DiaryPic from "./DiaryPic";
+import "./Diary.css";
 
 const Diary = () => {
   return (
     <div className="Diary">
       <div className="DiaryContainer">
         <Sidebar />
-        <div className='DiaryMain'>
-          <SearchBar />
-          <div className='DiaryDashboard'>
+        <div className="DiaryMain">
+          <div className="DiaryDashboard">
             <DiaryPic />
           </div>
+
+          <SearchBar />
         </div>
       </div>
     </div>
