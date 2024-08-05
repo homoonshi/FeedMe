fb5a05ac8eaa4bfcd68fbc0f4ffe6d70340c7151
unknown <jhsssafy@gmail.com>
2024-08-05 22:36:01 +0900

    Design : Feed

--- Front-feedme/src/components/Feed/FeedList.jsx
+++ Front-feedme/src/components/Feed/FeedList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './FeedList.css';
-import { FaAngleLeft, FaAngleRight, FaEllipsisH } from 'react-icons/fa';
+import { FaAngleLeft, FaAngleRight, FaEllipsisH, FaHeart } from 'react-icons/fa';
 import '../../assets/font/Font.css' 
 import photo1 from '../../assets/images/test1.png';
 import photo2 from '../../assets/images/test2.png';
@@ -8,10 +8,15 @@ import photo3 from '../../assets/images/test3.png';
 import '../../assets/font/Font.css' 
 
 const initialPhotos = [
-  { src: photo1, caption: '불사조', comments: [] },
-  { src: photo2, caption: '산책', comments: [] },
-  { src: photo3, caption: '판다', comments: [] },
+  { src: photo1, caption: '불사조', comments: [], author: '작성자1', time: '8월 1일 12:00', likes: 0 },
+  { src: photo2, caption: '산책', comments: [], author: '작성자2', time: '8월 2일 14:30', likes: 0 },
+  { src: photo3, caption: '판다', comments: [], author: '작성자3', time: '8월 3일 16:45', likes: 0 },
 ];
+
+const formatDate = (date) => {
+  const options = { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+  return new Date(date).toLocaleDateString('ko-KR', options);
+};
 
 const FeedList = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -37,7 +42,12 @@ const FeedList = () => {
     e.preventDefault();
     if (newComment.trim() !== '') {
       const updatedPhotos = [...photos];
-      updatedPhotos[currentIndex].comments.push(newComment);
+      const commentData = {
+        text: newComment,
+        author: '댓글작성자', 
+        time: formatDate(new Date()) 
+      };
+      updatedPhotos[currentIndex].comments.push(commentData);
       setPhotos(updatedPhotos);
       setNewComment('');
     }
@@ -49,13 +59,13 @@ const FeedList = () => {
 
   const handleCommentEdit = (index) => {
     setEditingComment(index);
-    setEditedComment(photos[currentIndex].comments[index]);
+    setEditedComment(photos[currentIndex].comments[index].text);
     setShowOptions(null);
   };
 
   const handleCommentSave = (index) => {
     const updatedPhotos = [...photos];
-    updatedPhotos[currentIndex].comments[index] = editedComment;
+    updatedPhotos[currentIndex].comments[index].text = editedComment;
     setPhotos(updatedPhotos);
     setEditingComment(null);
     setEditedComment('');
@@ -72,6 +82,12 @@ const FeedList = () => {
     setShowOptions(showOptions === index ? null : index);
   };
 
+  const handleLikeClick = () => { 
+    const updatedPhotos = [...photos];
+    updatedPhotos[currentIndex].likes += 1;
+    setPhotos(updatedPhotos);
+  };
+
   return (
     <div className="FeedList">
       <button className="arrowleft" onClick={handlePrevClick}>
@@ -79,18 +95,26 @@ const FeedList = () => {
       </button>      
       
       <div className="FeedListPhoto">
+        <div className="FeedListPhotoHeader">
+          <span className="FeedListPhotoauthor">{photos[currentIndex].author}</span> 
+          <span className="FeedListPhototime">{photos[currentIndex].time}</span> 
+        </div>
         <img src={photos[currentIndex].src} alt="feed" className="FeedListImg" />
         <p className="FeedListCaption">{photos[currentIndex].caption}</p>
+        <div className="FeedListPhotolikeSection"> 
+          <FaHeart onClick={handleLikeClick} className="FeedListPhotolikeButton" />
+          <span>{photos[currentIndex].likes}</span>
+        </div>
       </div>
 
       <div className="FeedListCom">
-        <form onSubmit={handleCommentSubmit} className="commentForm">
+        <form onSubmit={handleCommentSubmit} className="FeedListcommentForm">
           <input
             type="text"
             value={newComment}
             onChange={handleCommentChange}
             placeholder="댓글을 입력하세요..."
-            className="commentInput"
+            className="FeedListCommentInput"
           />
           <button type="submit" className="commentButton">등록</button>
         </form>
@@ -98,30 +122,33 @@ const FeedList = () => {
           {photos[currentIndex].comments.map((comment, index) => (
             <li key={index} className="commentItem">
               {editingComment === index ? (
-                <input
-                  type="text"
-                  value={editedComment}
-                  onChange={handleEditCommentChange}
-                  className="editCommentInput"
-                />
-              ) : (
-                <span>{comment}</span>
-              )}
-              <div className="commentActions">
-                {editingComment === index ? (
+                <div className="editCommentContainer"> 
+                  <input
+                    type="text"
+                    value={editedComment}
+                    onChange={handleEditCommentChange}
+                    className="editCommentInput"
+                  />
                   <button onClick={() => handleCommentSave(index)} className="saveButton">저장</button>
-                ) : (
-                  <>
+                </div>
+              ) : (
+                <>
+                  <div className="commentHeader">
+                    <span className="commentAuthor">{comment.author}</span>
+                    <span className="commentTime">{comment.time}</span>
+                  </div>
+                  <div className="commentBody">
+                    <span className="commentText">{comment.text}</span>
                     <FaEllipsisH onClick={() => handleShowOptions(index)} className="ellipsisButton" />
-                    {showOptions === index && (
-                      <div className="optionsDropdown">
-                        <button onClick={() => handleCommentEdit(index)} className="editButton">수정</button>
-                        <button onClick={() => handleCommentDelete(index)} className="deleteButton">삭제</button>
-                      </div>
-                    )}
-                  </>
-                )}
-              </div>
+                  </div>
+                </>
+              )}
+              {showOptions === index && (
+                <div className="optionsDropdown">
+                  <button onClick={() => handleCommentEdit(index)} className="editButton">수정</button>
+                  <button onClick={() => handleCommentDelete(index)} className="deleteButton">삭제</button>
+                </div>
+              )}
             </li>
           ))}
         </ul>
