c39b4fce80868722c5a8796d942b484005d222ca
장현수 <wkdalsrud113@naver.com>
2024-08-08 11:05:12 +0900

    Fix : loginloding

--- Front-feedme/src/components/Login/LoginLoding.jsx
+++ Front-feedme/src/components/Login/LoginLoding.jsx
@@ -5,14 +5,16 @@ import "./LoginLoding.css";
 const LoginLoding = () => {
   return (
     <div className="LoginLoding">
-      <div className="wrap">
-        <motion.div
-          className="box"
-          animate={{ scale: [1, 1.5, 1.1] }}
-          transition={{ duration: 3, times: [0, 0.2, 1] }}
-        >
-          Loding
-        </motion.div>
+      <div className="LoginLodingMain">
+        <div className="LoginLodingWrap">
+          <motion.div
+            className="LoginLodingMainBox"
+            animate={{ scale: [1, 1.5, 1.1] }}
+            transition={{ duration: 3, times: [0, 0.2, 1], repeat: Infinity }}
+          >
+            Loding
+          </motion.div>
+        </div>
       </div>
     </div>  
   );
