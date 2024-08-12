fcc3dc18407f5e072b40d210d1cba77a89f4f841
jingik <gch05926@gmail.com>
2024-08-13 04:49:48 +0900

    fix:redis

--- Backend-feedme/feedme/src/main/java/com/todoslave/feedme/config/security/RedisConfig.java
+++ Backend-feedme/feedme/src/main/java/com/todoslave/feedme/config/security/RedisConfig.java
@@ -1,29 +1,30 @@
 package com.todoslave.feedme.config.security;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
+ 
 @Configuration
-@EnableRedisRepositories
-@RequiredArgsConstructor
 public class RedisConfig {
+ 
+    @Value("${spring.redis.host}")
+    private String redisHost;
 
-    private final RedisProperties redisProperties;
+    @Value("${spring.redis.port}")
+    private String redisPort;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(redisProperties.getHost());
-        redisStandaloneConfiguration.setPort(redisProperties.getPort());
-        // redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+    redisStandaloneConfiguration.setHostName(redisHost);
+    redisStandaloneConfiguration.setPort(Integer.parseInt(redisPort));
+    // redisStandaloneConfiguration.setPassword(redisPassword);
+    LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+    return lettuceConnectionFactory;
     }
 
     @Bean
