de9c7a38fe4c8c5e6393ab1fa1cda41984ba9b35
김진기 <kjg6462@naver.com>
2024-08-12 14:08:43 +0900

    Test:server

--- Backend-feedme/feedme/src/main/java/com/todoslave/feedme/config/security/SecurityConfig.java
+++ Backend-feedme/feedme/src/main/java/com/todoslave/feedme/config/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                         .requestMatchers("/token/**").permitAll()
                         .requestMatchers("/api/**").permitAll()
                         .requestMatchers("/", "/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
-                        .requestMatchers("/login/oauth2/code/**","/login/**","/testsite" ,"/signup", "/user", "/v3/api-docs/**", "/swagger-ui/**","/creature", "/swagger-ui.html", "/users/**").permitAll() // 유저 설정
+                        .requestMatchers("/api/login/oauth2/code/**","/api/login/**","/api/testsite" ,"/api/signup", "/api/user", "/api/v3/api-docs/**", "/api/swagger-ui/**","/api/creature", "/api/swagger-ui.html", "/api/users/**").permitAll() // 유저 설정
                         .requestMatchers("/ws/**").permitAll()
                         .anyRequest().authenticated()
                 )
