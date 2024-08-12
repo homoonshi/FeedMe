c306009d01b1378deb0613240d29d7830256e207
장현수 <wkdalsrud113@naver.com>
2024-08-12 09:09:31 +0900

    Fix : merge mon

--- Front-feedme/src/store/store.js
+++ Front-feedme/src/store/store.js
@@ -3,27 +3,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slice';
 import userReducer from './userSlice';
-<<<<<<< HEAD
 import alarmReducer from './alarmSlice';
 import todoReducer from './todoSlice';
-=======
 import friendsListReducer from './friendsSlice.js';
 import friendInfoReducer from './friendInfoSlice.js'; 
 import feedListReducer from './feedListSlice.js';
->>>>>>> origin/front-hyunsu
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
-<<<<<<< HEAD
     alarm: alarmReducer,
     todo: todoReducer,
-=======
     friendsList: friendsListReducer,
     friendInfo: friendInfoReducer,
     feedList: feedListReducer, 
->>>>>>> origin/front-hyunsu
   },
 });
 
