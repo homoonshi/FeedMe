b23779f277dd90c0a305b4cf84ade4dd0330ce46
김문희 <homoonshi@gmail.com>
2024-08-13 11:14:24 +0900

    Fix : SSE 관련 설정 변경

--- Backend-feedme/feedme/src/main/java/com/todoslave/feedme/config/security/SecurityConfig.java
+++ Backend-feedme/feedme/src/main/java/com/todoslave/feedme/config/security/SecurityConfig.java
@@ -43,9 +43,10 @@ public class SecurityConfig {
                         .requestMatchers("/token/**", "/bot/**").permitAll()
                         .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**").permitAll()
                         .requestMatchers("/login/oauth2/code/**", "/login/**", "/testsite", "/signup", "/user", "/v3/api-docs/**",
-                                "/swagger-ui/**", "/creature", "/swagger-ui.html", "/users/**","/alarms/**").permitAll() // 유저 설정
-                        .requestMatchers("/ws/**").permitAll()
-                        .anyRequest().authenticated()
+                                "/swagger-ui/**", "/creature", "/swagger-ui.html", "/users/**").permitAll() // 유저 설정
+                        .requestMatchers("/ws/**","/alarms/**").permitAll()
+                        .anyRequest()
+                        .authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("https://i11b104.p.ssafy.io")
