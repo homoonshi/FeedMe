2c70f351df274666cdea0d6cb63f5555d03c080b
unknown <wkdalsrud113@naver.com>
2024-08-14 15:41:38 +0900

    Fix : server host

--- Front-feedme/src/components/Diary/DiaryPic.jsx
+++ Front-feedme/src/components/Diary/DiaryPic.jsx
@@ -23,7 +23,7 @@ function DiaryPic() {
 
   const fetchDiaries = async (page, limit) => {
     try {
-      const response = await axios.post('http://localhost:8080/diary/list', {
+      const response = await axios.post('https://i11b104.p.ssafy.io/api/diary/list', {
         skip: page * limit,
         limit: limit,
       },
@@ -109,7 +109,7 @@ function DiaryPic() {
     }
 
     try {
-      const response = await axios.post('http://localhost:8080/feed', {
+      const response = await axios.post('https://i11b104.p.ssafy.io/api/feed', {
         diaryDate: currentDate,
         content: feedContent,
       }, {
