dc041bca7cd8d3ffc4e3f9e49f88f933105ee70b
김진기 <kjg6462@naver.com>
2024-08-15 02:26:53 +0900

    Fix:server

--- Front-feedme/src/components/WebScoketChat/WebSocketChat.jsx
+++ Front-feedme/src/components/WebScoketChat/WebSocketChat.jsx
@@ -24,7 +24,7 @@ const ChatRoom = ({ roomId }) => { // roomId를 props로 받도록 수정
   const connect = () => {
     if (stompClient.current) return; // 이미 연결되어 있는지 확인
 
-    const socket = new SockJS('http://localhost:8080/ws/friendChat');
+    const socket = new SockJS('https://i11b104.p.ssafy.io/api/ws/friendChat');
     stompClient.current = new Client({
         webSocketFactory: () => socket,
         debug: (str) => {
