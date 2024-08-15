3346faea7da6228794bb394b4ca22323be2f30f7
장현수 <wkdalsrud113@naver.com>
2024-08-16 04:47:29 +0900

    Fix : url

--- Front-feedme/src/components/Creature/CreatureResult.jsx
+++ Front-feedme/src/components/Creature/CreatureResult.jsx
@@ -6,6 +6,11 @@ import { faAngleLeft, faRotate } from '@fortawesome/free-solid-svg-icons';
 import './CreatureResult.css';
 import '../../assets/font/Font.css';
 import { fetchUserData } from '../../store/userSlice';
+import eg1 from '../../assets/images/1.gif'
+import eg2 from '../../assets/images/2.gif'
+import eg3 from '../../assets/images/3.gif'
+import eg4 from '../../assets/images/4.gif'
+import eg5 from '../../assets/images/5.gif'
 
 const CreatureResult = () => {
   const dispatch = useDispatch();
@@ -21,8 +26,13 @@ const CreatureResult = () => {
   }, [dispatch, token]);
   
   const { creatureName, photo } = useSelector((state) => state.auth);
+  const getRandomEggImage = () => {
+    const eggImages = [eg1, eg2, eg3, eg4, eg5];
+    return eggImages[Math.floor(Math.random() * eggImages.length)];
+  };
+
+  const displayImage = photo ? `data:image/gif;base64,${photo}` : getRandomEggImage();
   
-  console.log(user)
   return (
     <div className="CreatureResultMain">
       <div className="CreatureResultContainer">
@@ -33,7 +43,7 @@ const CreatureResult = () => {
           <div className="CreatureResultName">{creatureName}</div>
         </div>
         <div className="CreatureResultHeader">
-          <img src={`data:image/gif;base64,${photo}`} alt="Cat" className="CreatureResultImage" />
+          <img src={displayImage} alt="Creature" className="CreatureResultImage" />
         </div>
         <Link to="/Main">
           <button type="submit" className="CreatureResultStartButton">Start</button>
