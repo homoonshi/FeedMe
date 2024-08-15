553d8e9c5321d5397425c47e78fbbe6e75753eda
yebon <goodyebon1@gmail.com>
2024-08-15 14:50:21 +0900

    test : 서버 테스트 2

--- Backend-feedme/feedme/src/main/java/com/todoslave/feedme/util/FlaskClientUtil.java
+++ Backend-feedme/feedme/src/main/java/com/todoslave/feedme/util/FlaskClientUtil.java
@@ -99,16 +99,18 @@ public class FlaskClientUtil {
      * @param level     크리쳐의 현재 레벨 (nullable)
      * @return Flask 서버로부터 받은 이미지 데이터 (byte[])
      */
+
+    //크리쳐 이미지 당겨오기
     public byte[] getCreatureImageAsByteArray(String username, Integer creatureId, Integer level) {
         String url;
 
         // 조건에 따라 URL 결정
         if (creatureId == null || creatureId == 0 || level == null || level == 0) {
             // creatureId나 level이 null 또는 0인 경우
-            url = String.format("http://localhost:3333/store/default_creature_image/%s", username);
+            url = String.format("http://localhost:33333/store/default_creature_image/%s", username);
         } else {
             // 일반적인 경우
-            url = String.format("http://localhost:3333/store/creature_data/%s/%d?level=%d", username, creatureId, level);
+            url = String.format("http://localhost:33333/store/creature_data/%s/%d/%d", username, creatureId, level);
         }
 
         // GET 요청을 통해 Flask 서버로부터 이미지 데이터 받기
@@ -129,12 +131,13 @@ public class FlaskClientUtil {
      * @param date     그림일기를 요청하는 날짜 (LocalDate 형식)
      * @return Flask 서버로부터 받은 그림일기 데이터 (byte[])
      */
+    //크리쳐 다이어리 가져오기
     public byte[] getCreatureDiaryAsByteArray(String username, LocalDate date) {
         // 날짜를 문자열 형식으로 변환 (yyyy-MM-dd)
         String formattedDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
         // URL 생성 (날짜는 쿼리 파라미터로 전송)
-        String url = String.format("http://localhost:3333/store/creature_diary/%s?date=%s", username, formattedDate);
+        String url = String.format("http://localhost:33333/store/creature_diary/%s/%s", username, formattedDate);
 
         // GET 요청을 통해 Flask 서버로부터 그림일기 데이터 받기
         ResponseEntity<ByteArrayResource> response = restTemplate.getForEntity(url, ByteArrayResource.class);
