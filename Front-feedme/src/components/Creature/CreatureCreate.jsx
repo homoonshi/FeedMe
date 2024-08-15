49aaa0c137872be5fb4d2f1c8886c7aa25626ce4
박예본 <goodyebon123@naver.com>
2024-08-15 19:57:57 +0900

    Test:imageResizing

--- Front-feedme/src/components/Creature/CreatureCreate.jsx
+++ Front-feedme/src/components/Creature/CreatureCreate.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch, useSelector } from 'react-redux';
-import { setCreatureName, setKeyword, addKeyword, removeKeyword, resetKeywords, } from '../../store/slice';
+import { setCreatureName, setKeyword } from '../../store/slice';
 import './CreatureCreate.css';
 import '../../assets/font/Font.css';
 import axios from 'axios';
@@ -14,94 +14,65 @@ const CreatureCreate = () => {
   const { creatureName, keyword, token } = useSelector((state) => state.auth);
   const [photo, setPhoto] = useState(null); // local state로 photo 관리
 
-  const handleImageUpload = (e) => {
+  // 이미지 리사이징 함수
+  const resizeImage = (file, maxWidth, maxHeight) => {
+    return new Promise((resolve) => {
+      const img = new Image();
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        img.src = e.target.result;
+
+        img.onload = () => {
+          let width = img.width;
+          let height = img.height;
+
+          console.log('Original Image Size:', width, 'x', height);  // 원본 이미지 크기 출력
+
+          // 이미지 크기를 720으로 리사이즈
+          if (width > height && width > maxWidth) {
+            height = Math.floor((height *= maxWidth / width));
+            width = maxWidth;
+          } else if (height > width && height > maxHeight) {
+            width = Math.floor((width *= maxHeight / height));
+            height = maxHeight;
+          } else if (width > maxWidth) {
+            width = maxWidth;
+            height = maxHeight;
+          }
+
+          console.log('Resized Image Size:', width, 'x', height);  // 리사이즈된 이미지 크기 출력
+
+          canvas.width = width;
+          canvas.height = height;
+          ctx.drawImage(img, 0, 0, width, height);
+          canvas.toBlob(resolve, file.type);
+        };
+      };
+
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const handleImageUpload = async (e) => {
     const file = e.target.files[0];
     if (file) {
-      setPhoto(file); // 로컬 상태에 파일 저장
+      // 이미지 리사이징 수행
+      const resizedImage = await resizeImage(file, 1024, 1024);
+      setPhoto(resizedImage); // 리사이징된 파일로 photo 상태 업데이트
     }
   };
 
-  // const handleKeywordChange = (type, value) => {
-  //   const keyword = { type, value };
-  //   const isKeywordExist = keywords.some((kw) => kw.type === type);
-
-  //   if (isKeywordExist) {
-  //     // 이미 존재하는 키워드 타입을 제거하고 새로운 값으로 추가
-  //     dispatch(removeKeyword({ type }));
-  //   }
-
-  //   dispatch(addKeyword(keyword));
-  // };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // const formData = new FormData();
-    // formData.append('creatureName', creatureName);
-    // formData.append('keywords', JSON.stringify(keywords)); // 키워드 배열 직렬화
-    // if (photo) {
-    //   formData.append('photo', photo.name); // 파일 추가
-    // }
-
-    // const formData = new FormData();
-    // formData.append('creatureName', creatureName);
-    // formData.append('photo', photo.name);
-    // formData.append('keyword', keyword);
-
-    // // FormData를 JSON으로 변환하는 함수
-    // function formDataToJson(formData) {
-    //   const obj = {};
-    //   formData.forEach((value, key) => {
-    //     if (value instanceof File) {
-    //       // 파일 메타데이터를 JSON으로 포함
-    //       obj[key] = {
-    //         name: value.name,
-    //         size: value.size,
-    //         type: value.type,
-    //       };
-    //     } else {
-    //       obj[key] = value;
-    //     }
-    //   });
-    //   return JSON.stringify(obj);
-    // }
-
-    // const json = formDataToJson(formData);
-
-    console.log(typeof photo);
-    console.log(creatureName);
-    console.log(keyword);
-    console.log(token);
-
-    // // 객체 생성
-    // const creatureData = {
-    //   creatureName: creatureName,
-    //   keyword: keyword,
-    //   photo: photo ? photo.name : null,  // photo가 선택된 경우 파일 이름을 전달
-    // };
-
-    // try {
-    //   const response = await axios.post('https://i11b104.p.ssafy.io/api/creature', creatureData, {
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       'Authorization': sessionStorage.getItem('accessToken'), // 또는 token 변수를 사용
-    //     },
-    //   });
-
-    //   if (response.status === 200) {
-    //     navigate('/CreatureResult');
-    //   } else {
-    //     console.log('크리쳐 생성 실패', response.data);
-    //   }
-    // } catch (error) {
-    //   console.error('서버 요청 중 오류 발생', error);
-    // }
 
     const formData = new FormData();
     formData.append('creatureName', creatureName);
     formData.append('keyword', keyword);
     if (photo) {
-      formData.append('file', photo); // 파일을 FormData에 추가
+      formData.append('file', photo); // 리사이즈된 파일을 FormData에 추가
     }
 
     try {
@@ -121,29 +92,6 @@ const CreatureCreate = () => {
     } catch (error) {
       console.error('서버 요청 중 오류 발생', error);
     }
-
-    // // 서버에 JSON 형식으로 데이터 보내기
-    // try {
-    //   const response = await axios.post('https://i11b104.p.ssafy.io/api/creature', {
-    //     creatureName : creatureName,
-    //     keyword : keyword,
-    //     photo: photo.name  
-    //   }, {
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       'Authorization': sessionStorage.getItem('accessToken'),
-    //     }
-    //   });
-
-    //   if (response.status === 200) {
-    //     navigate('/CreatureResult');
-    //   } else {
-    //     console.log('크리쳐 생성 실패', response.data);
-    //   }
-    // } catch (error) {
-    //   console.error('서버 요청 중 오류 발생', error);
-    // }
-
   };
 
   return (
@@ -181,144 +129,6 @@ const CreatureCreate = () => {
               <img src={URL.createObjectURL(photo)} alt="Preview" className="CreatureCreateFormImagePreview" />
             </div>
           )}
-
-          {/* <div className="CreatureCreateFormKeywordsContainer">
-            <label className="CreatureCreateFormKeywordsHeader">키워드</label>
-            <div className="CreatureCreateFormKeywords">
-              <div>
-                <div>개체 선택</div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="type"
-                    value="강아지"
-                    onChange={(e) => handleKeywordChange('type', e.target.value)}
-                  />
-                  <label htmlFor="dog">강아지</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="type"
-                    value="고양이"
-                    onChange={(e) => handleKeywordChange('type', e.target.value)}
-                  />
-                  <label htmlFor="cat">고양이</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="type"
-                    value="판다"
-                    onChange={(e) => handleKeywordChange('type', e.target.value)}
-                  />
-                  <label htmlFor="panda">판다</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="type"
-                    value="래서 판다"
-                    onChange={(e) => handleKeywordChange('type', e.target.value)}
-                  />
-                  <label htmlFor="red panda">래서 판다</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="type"
-                    value="여자 아이"
-                    onChange={(e) => handleKeywordChange('type', e.target.value)}
-                  />
-                  <label htmlFor="girl">여자 아이</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="type"
-                    value="남자 아이"
-                    onChange={(e) => handleKeywordChange('type', e.target.value)}
-                  />
-                  <label htmlFor="a boy">남자 아이</label>
-                </div>
-              </div>
-              <div>
-                <div>색상 선택</div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="빨간색"
-                    onChange={(e) => handleKeywordChange('color', e.target.value)}
-                  />
-                  <label htmlFor="red">빨간색</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="노란색"
-                    onChange={(e) => handleKeywordChange('color', e.target.value)}
-                  />
-                  <label htmlFor="yellow">노란색</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="흰색"
-                    onChange={(e) => handleKeywordChange('color', e.target.value)}
-                  />
-                  <label htmlFor="white">흰색</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="보라색"
-                    onChange={(e) => handleKeywordChange('color', e.target.value)}
-                  />
-                  <label htmlFor="purple">보라색</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="금색"
-                    onChange={(e) => handleKeywordChange('color', e.target.value)}
-                  />
-                  <label htmlFor="gold">금색</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="분홍색"
-                    onChange={(e) => handleKeywordChange('color', e.target.value)}
-                  />
-                  <label htmlFor="pink">분홍색</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="은색"
-                    onChange={(e) => handleKeywordChange('color', e.target.value)}
-                  />
-                  <label htmlFor="silver">은색</label>
-                </div>
-                <div className="CreatureCreateFormKeywordOption">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="회색"
-                    onChange={(e) => handleKeywordChange('color', e.target.value)}
-                  />
-                  <label htmlFor="grey">회색</label>
-                </div>
-              </div>
-            </div>
-          </div> */}
           <button type="submit" className="CreatureCreateButton">생성</button>
         </form>
       </div>
