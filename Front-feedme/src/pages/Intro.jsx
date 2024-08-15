4059392b7e655cb792fd87edb59303c60c636d68
김문희 <homoonshi@gmail.com>
2024-08-15 22:33:37 +0900

    Fix : 메인 비동기 -> navigate 수정

--- Front-feedme/src/pages/Intro.jsx
+++ Front-feedme/src/pages/Intro.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../pages/Intro.css';
 import '../assets/font/Font.css';
 
 const Intro = () => {
-  const [redirectPath, setRedirectPath] = useState('/Login');
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -23,25 +22,21 @@ const Intro = () => {
         if (payload.exp < currentTime) {
           console.log('토큰이 만료되었습니다.');
           sessionStorage.removeItem('accessToken');
-          setRedirectPath('/Login');
+          navigate('/Login'); // 바로 로그인 페이지로 리디렉션
         } else {
           console.log('토큰이 유효합니다.');
-          setRedirectPath('/Main');
+          navigate('/Main'); // 바로 메인 페이지로 리디렉션
         }
       } catch (e) {
         console.error('토큰 파싱 중 오류 발생:', e);
         sessionStorage.removeItem('accessToken');
-        setRedirectPath('/Login');
+        navigate('/Login'); // 오류 발생 시 로그인 페이지로 리디렉션
       }
     } else {
       console.log('토큰이 존재하지 않습니다.');
-      setRedirectPath('/Login');
+      navigate('/Login'); // 토큰이 없으면 로그인 페이지로 리디렉션
     }
   };
-
-  useEffect(() => {
-    navigate(redirectPath);
-  }, [redirectPath, navigate]);
 
   return (
     <div className="Intro">
