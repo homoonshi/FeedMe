d95e45b89aa85b4c023b84acda9b243db6afbbd7
yebon <goodyebon1@gmail.com>
2024-08-16 03:38:16 +0900

    test

--- Backend-feedme/feedme/src/main/java/com/todoslave/feedme/util/FlaskClientUtil.java
+++ Backend-feedme/feedme/src/main/java/com/todoslave/feedme/util/FlaskClientUtil.java
@@ -109,25 +109,15 @@ public class FlaskClientUtil {
         String url;
 
         // URL 생성: username을 creatureId로 간주하여 사용
-<<<<<<< HEAD
         if (creatureId == null || creatureId == 0 || level == null || level == 0) {
             int tmp = username.length() % 4;
             // creatureId나 level이 null 또는 0인 경우, 기본 이미지를 요청
             url = String.format("http://flask:33333/store/egg/0/%d",tmp);
 
         } else {
-=======
-        // if (creatureId == null || creatureId == 0 || level == null || level == 0) {
-        //     // creatureId나 level이 null 또는 0인 경우, 기본 이미지를 요청
-        //     String numberString = username.replaceAll("[^0-9]", "");
-        //     int number = Integer.parseInt(numberString);
-
-        //     url = String.format("http://flask:33333/store/egg/0/%d", number%4);
-        // } else {
->>>>>>> c530a58094c0e3e727888491b0e55f82a7cb94be
             // 일반적인 경우
             url = String.format("http://flask:33333/store/%s/%d/%d", username, creatureId, level);
-        // }
+        }
 
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
